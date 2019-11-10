d4c782bbbe410db72138c86a81040a9252ef1fe7
Nuria Lamonja @needlesslord <needlesslord@gmail.com>
2019-11-10 13:21:07 +0100

    bye

--- Apunts uni/- Alex-Enric -/Physics/Pokemon_Pinball_Sapphire-master/Pokemon_Pinball_Sapphire-master/README.md
+++
@@ -1,2 +0,0 @@
-# Pokemon_Pinball_Sapphire
- https://www.spriters-resource.com/game_boy_advance/pokemonpinballrubysapphire/
--- Apunts uni/- Victor-Oriol -/Victor_Segura_Oriol_Capdevila-master/Victor_Segura_Oriol_Capdevila-master/Pinball/Flecha.cpp
+++
@@ -1,8 +0,0 @@
-#include "Application.h"
-#include "Globals.h"
-#include "Flecha.h"
-
-Flecha::Flecha(int x, int y, SDL_Texture* thistexture) : Cosa(x, y)
-{
-	tex = thistexture;
-}
